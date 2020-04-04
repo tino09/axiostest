14a3da0b52f4a329c55313c480bfbc4adafad6a2
Valentine Ayim <valentineayimodu@gmail.com>
2020-04-04 14:01:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ document.getElementById('transform').addEventListener('click', transformResponse
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
 
+// Axios GLobals
+axios.defaults.headers.common['X-Auth-Token'] = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 function getTodos() {
     /*axios({
         method:'get',
@@ -25,7 +29,9 @@ function getTodos() {
   //      .then((data)=> console.log(data));
 
   axios
-  .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+      timeout : 5000
+  })
   .then(res=>{console.log(res)})
   .catch(err => console.log(err))
   
@@ -94,9 +100,7 @@ function getData() {
         axios.get('https://jsonplaceholder.typicode.com/todos'),
         axios.get('https://jsonplaceholder.typicode.com/posts')
     ])
-    .then(res => {
-        console.log(res[0]);
-        console.log(res[1]);
+    .then(axios.spread(todos, posts) => console.log(posts)
     })
     .catch(err => console.error(err)) */
 
@@ -112,21 +116,80 @@ function getData() {
 }
 
 function customHeaders() {
+    const config = {
+        headers: {
+            'content-type' : 'Application/json',
+            Authorization : 'sometoken'
     
+        }
+    }
     
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', config)
+    .then(res => { console.log(res)})
+    .catch(err => console.error(err));
 }
 
 function errorHandling() {
-    
-    
+    axios
+    .get('https://jsonplaceholder.typicode.com/todoss', {
+        validateStatus : function(status){
+            return status < 500; // 
+        }
+    })
+    .then(res => console.log(res))
+    .catch(err => {
+        if(err.response){
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+
+            if (err.response.status === 404) {
+                alert('Error: Page Not Found')
+            }
+
+        }else if(err.request){
+            // Request was made but no response
+            console.error(err.request);
+        }else{
+            console.error(err.message);
+        }
+    });
 }
 
 function cancelToken() {
+    const source = axios.CancelToken.source();
+
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos', {
+        cancelToken : source.token
+    })
+    .then(res=> {console.log(res)})
+    .catch(thrown => {
+        if(axios.isCancel(thrown)) {
+            console.log('Request canceled', thrown.message);
+        }
+    });
+
+    if (true) {
+        source.cancel('Cancelled!')
+    }
     
 }
 
-function transformResponse(params) {
-    
+function transformResponse() {
+    const options = {
+        method : 'post',
+        url :'https://jsonplaceholder.typicode.com/todos',
+        data : {
+            title : "picnic panic"
+        },
+        transformResponse : axios.defaults.transformResponse.concat(data => {
+            data.title = data.title.toUpperCase();
+            return data;
+        })
+    }
+    axios(options).then(res=>{console.log(res)});
 }
 
 // Intercepting requests & Responses
@@ -139,4 +202,31 @@ axios.interceptors.request.use(config => {
     
 }, error => {
     return Promise.reject(error);
+});
+
+// Axios Instances
+
+const axiosInstance = axios.create({
+    baseURL:'https://jsonplaceholder.typicode.com'
+});
+
+axiosInstance
+.get('/comments')
+.then(res => {console.log(res)})
+.catch(err => {
+    if(err.response){
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+
+        if (err.response.status === 404) {
+            alert('Error: Page Not Found')
+        }
+
+    }else if(err.request){
+        // Request was made but no response
+        console.error(err.request);
+    }else{
+        console.error(err.message);
+    }
 });
